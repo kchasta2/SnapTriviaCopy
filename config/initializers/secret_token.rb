ab8324b6002902255e70615f01ffa1e64282d40b
David Siver <dsiver1@my.westga.edu>
2015-02-11 13:32:37 -0500

    removed secret token from gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snaptrivia::Application.config.secret_key_base = 'cd4d58e17da1e795dfa44363fc14cba4a4045df4c82427b7e7d84d3c09f516e5941d45cd580e16b44b66c460291eb614270b759baf91919c0308fd732bf5a4dd'
