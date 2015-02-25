280949255b15abcc51a78e67a7c8373cdbb589f2
David Siver <dsiver1@my.westga.edu>
2015-02-25 12:44:48 -0500

    changed ability class admin privileges

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,11 @@ class Ability
 
   def initialize(user)
     @user = user || User.new
-
     if(@user.admin)
       can :access, :rails_admin
       can :dashboard
-      can :manage, :all
+      can :manage, Question
+      can :manage, User
     end
     if(@user.reviewer)
       can :access, :rails_admin
