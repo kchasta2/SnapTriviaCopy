b7406714508911b22e5c24c184c1b289ebef32ea
B3Chill <B3Chill@gmail.com>
2015-03-12 01:52:31 -0400

    spinner grabs question and displays, working on detecting right and wrong and

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,7 +21,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    
+
   end
 
   def create
@@ -31,8 +31,7 @@ class QuestionsController < ApplicationController
     redirect_to @question
   end
 
-
-
+  
   private
   def question_params
     params.require(:question).permit( :title, :rightAns, :wrongAns1, :wrongAns2, :wrongAns3, :subject_title)
