95ece60946b203d3f9d89f4883b3638fd2b44660
B3Chill <B3Chill@gmail.com>
2015-03-12 02:54:59 -0400

    fixed passing of index on game/show

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,9 @@
 class Game < ActiveRecord::Base
-#  belongs_to :player1, :class_name => 'User', :foreign_key => 'player1_id'
-#  belongs_to :player2, :class_name => 'User', :foreign_key => 'player2_id'
+  belongs_to :player1, :class_name => 'User', :foreign_key => 'player1_id'
+  belongs_to :player2, :class_name => 'User', :foreign_key => 'player2_id'
 
-#  validates :player1_id, presence: true
-#  validates :player2_id, presence: true
+  validates :player1_id, presence: true
+  validates :player2_id, presence: true
 
 
 
