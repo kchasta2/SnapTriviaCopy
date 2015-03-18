392fa7e26b22c6258e36f8099e7fc23b16097364
B3Chill <B3Chill@gmail.com>
2015-03-18 19:59:14 -0400

    working on views index now works and shows games if games are in db

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,31 +1,35 @@
 class GameController < ApplicationController
 
   def index
-      end
-
-      def show
-        subject_title = params[:subject_title]
-        @question = Question.find_by_subject_title(subject_title)
-
-        respond_to do |format|
-          format.html
-          format.xml { render :xml => @question }
-        end
-      end
-
-      def create
-        @player1 = current_user
-        @player2 = :player2
-      end
-
-      def new
-        @game = Game.new(game_params)
-      end
-
-      private
-      def game_params
-        params.require(:game).permit(current_user.id, @player2.id, game_over: false, approved: true, player1_turn: true)
-      end
   end
+
+  def show
+    subject_title = params[:subject_title]
+    @question = Question.find_by_subject_title(subject_title)
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @question }
+    end
+  end
+
+  def ask_question
+
+  end
+
+  def create
+    @player1 = current_user
+    @player2 = :player2
+  end
+
+  def new
+    @game = Game.new(game_params)
+  end
+
+  private
+  def game_params
+    # params.require(:game).permit(current_user.id, @player2.id, game_over: false, approved: true, player1_turn: true)
+  end
+end
 
 
