20363e58ae0f1a5b1218ba61ded8c3c29d70867c
David Siver <dsiver1@my.westga.edu>
2015-03-19 19:17:34 -0400

    edited create method of game controller to send msgs to reviewers after question is created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,9 +21,9 @@ class QuestionsController < ApplicationController
 
   # creates new question checks params
   def create
-  @question = Question.new(question_params)
-
+    @question = Question.new(question_params)
     @question.save
+    notify_reviewers
     redirect_to @question
   end
 
@@ -31,5 +31,13 @@ class QuestionsController < ApplicationController
   private
   def question_params
     params.require(:question).permit( :title, :rightAns, :wrongAns1, :wrongAns2, :wrongAns3, :subject_title)
+  end
+
+  def notify_reviewers
+    User.where(reviewer: true).find_each do |user|
+      message = Message.create( {:sender_id => 0, :subject => 'New Question', :body => @question.id, :sender_name => 'system', :recipient_name => user.name, :recipient_id => user.id } )
+      message.save
+    end
+    flash[:notice] = 'Reviewers have been notified. Your question is pending'
   end
 end
