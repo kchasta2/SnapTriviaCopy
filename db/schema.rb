8ceeeda52fb896395b9412d33f7d46ccf34677f0
B3Chill <B3Chill@gmail.com>
2015-04-01 16:05:33 -0400

    fixing schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20150320223041) do
-=======
+
 ActiveRecord::Schema.define(version: 20150321224659) do
 
 
