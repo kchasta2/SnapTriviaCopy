f38d9f933b4d1a6ad54892703a838e97c0ec36fd
Kristi Chastain <kchasta2@my.westga.edu>
2015-04-01 23:29:26 -0400

    test ad

--- config/initializers/ads.rb
+++ config/initializers/ads.rb
@@ -1,8 +1,4 @@
 Ads.configure do |config|
-  config.renderer = lambda { |options|
-    tag(
-        :img,
-        src: "http://placehold.it/500x65/ffff00/000099&text=Adsense+Placeholder"
-    )
-  }
+  config.renderer = nil
+  
 end
